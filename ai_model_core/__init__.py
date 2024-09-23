1fa42c77c20c2ed61b9d63162bfbcd7eb04e0572
sytse06 <sytse@svdschaaf.nl>
2024-09-23 12:22:50 +0200

    Step 2 Getting dependencies ai_model_core right.

--- ai_model_core/__init__.py
+++ ai_model_core/__init__.py
@@ -1,13 +1,14 @@
 # ai_model_core/__init__.py
 from .factory import get_model, get_embedding_model
-from .utils import get_system_prompt, get_prompt_template, _format_history
+from .utils import get_system_prompt, get_prompt_template, _format_history, load_document, load_web_content, split_text
 from .config.credentials import load_credentials, get_api_key
 from .config.settings import load_config, get_directory, get_prompt, get_prompt_list, update_prompt_list
-from .model_helpers import ChatAssistant, RAGAssistant, VisionAssistant, PromptAssistant
+from .model_helpers import ChatAssistant, RAGAssistant, VisionAssistant, PromptAssistant, SummarizationAssistant
 from ai_model_core.model_helpers.RAG_assistant import CustomHuggingFaceEmbeddings
 
 __all__ = [
     'get_model', 'get_embedding_model', 'format_prompt', 'get_system_prompt', 'get_prompt_template', '_format_history',
     'load_credentials', 'get_api_key', 'load_config', 'get_directory', 'get_prompt', 'load_content',
-    'get_prompt_list', 'update_prompt_list', 'ChatAssistant', 'VisionAssistant', 'PromptAssistant', 'RAGAssistant', 'CustomHuggingFaceEmbeddings'
+    'get_prompt_list', 'update_prompt_list', 'ChatAssistant', 'VisionAssistant', 'PromptAssistant', 
+    'RAGAssistant', 'SummarizationAssistant', 'CustomHuggingFaceEmbeddings'
 ]
--- ai_model_core/model_helpers/__init__.py
+++ ai_model_core/model_helpers/__init__.py
@@ -3,5 +3,6 @@ from .chat_assistant import ChatAssistant
 from .vision_assistant import VisionAssistant
 from .prompt_assistant import PromptAssistant
 from .RAG_assistant import RAGAssistant
+from .summarize_assistant import SummarizationAssistant
 
-__all__ = ['ChatAssistant', 'VisionAssistant', 'PromptAssistant', 'RAGAssistant']
+__all__ = ['ChatAssistant', 'VisionAssistant', 'PromptAssistant', 'RAGAssistant', 'SummarizationAssistant']
