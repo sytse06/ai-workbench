7983e250e9deb698f05e1fbfa7bb9ee3fa6e5c0b
sytse06 <sytse@svdschaaf.nl>
2024-11-08 08:48:06 +0100

    Introduction of new dir shared utils.

---
+++ ai_model_core/shared_utils/__init__.py
@@ -0,0 +1,38 @@
+# ai_model_core/shared_utils/__init__.py
+from ..shared_utils.utils import (
+    get_system_prompt,
+    get_prompt_template,
+    _format_history,
+    EnhancedContentLoader
+)
+from ..shared_utils.factory import (
+    get_model, 
+    get_embedding_model
+)
+from ..config.credentials import (
+    load_credentials, 
+    get_api_key
+)
+from ..config.settings import (
+    load_config,
+    get_directory,
+    get_prompt,
+    get_prompt_list,
+    update_prompt_list
+)
+
+__all__ = [
+    'get_system_prompt',
+    'get_prompt_template',
+    'format_history',
+    'EnhancedContentLoader',
+    'get_model', 
+    'get_embedding_model',
+    'load_credentials', 
+    'get_api_key',
+    'load_config',
+    'get_directory',
+    'get_prompt',
+    'get_prompt_list',
+    'update_prompt_list'
+]
